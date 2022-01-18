d7d72163b159c29761f56615c16d8aa8beffc526
Nathaniel Sop <nathanielsop@gmail.com>
2022-01-18 13:11:00 -0500

    Container Update

--- portfolio/pages/index.tsx
+++ portfolio/pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet"/> 
         </head>
         <body className='bg-body text-white font-poppins'>
-          <div>
+          <div className='w-100'>
             <Navbar />
             <Hero />
             <div id="work">
