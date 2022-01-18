011d870ae7a654ac63d71baaa797ae024c985e82
Nathaniel Sop <nathanielsop@gmail.com>
2022-01-18 11:09:57 -0500

    First Actual Release
    - Many many design changes,
    
    - Completed the overall look and functionality for a starting portfolio
    
    Notes:
    
    Would like to get the social links at the bottom on one line for mobile view
    
    That's it for now

--- portfolio/pages/index.tsx
+++ portfolio/pages/index.tsx
@@ -1,24 +1,38 @@
 import Head from 'next/head'
 import Navbar from "../components/Navbar"
-import About from "../components/About"
-import Projects from "../components/Projects"
-import Education from "../components/Education"
+import Hero from "../components/Hero"
+import Work from "../components/Work"
 import Experience from "../components/Experience"
+import Links from "../components/Links"
 import Skills from "../components/Skills"
-
 
 export default function Home() {
   return (
-    <div className="h-screen bg-black">
-      { /* Navbar */ }
-      <div className="static">
-        <Navbar />
-      </div>
+    <html>
+        <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com"/>
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet"/> 
+        </head>
+        <body className='bg-body text-white font-poppins'>
+          <div>
+            <Navbar />
+            <Hero />
+            <div id="work">
+            <Work />
+            </div>
 
-      <div className='bg-gray-900 w-100 h-1/2'>
-
-      </div>
-
-    </div>
+            <div id="experience">
+            <Experience />
+            </div>
+            <div id="skills">
+            <Skills />
+            </div>
+            <div id="links">
+            <Links />
+            </div>
+          </div>
+        </body>
+    </html>
   )
 }
