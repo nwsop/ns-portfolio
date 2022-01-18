641c66cc2bb4831df67f542f568feff2357bb4d2
Nathaniel Sop <nathanielsop@gmail.com>
2022-01-18 12:52:18 -0500

    Skills.js updated (#2)

--- portfolio/components/Skills.js
+++ portfolio/components/Skills.js
@@ -7,54 +7,54 @@ const Skills = () => {
 			<section className="w-full">
 				<h2 id="skills" class="secondary-title text-8xl font-bold">Skills</h2>
 
-				<div className='flex flex-wrap grid-cols-1 md:grid-cols-2 lg:grid-cols-2 w-full'>
+				<div className='flex flex-wrap grid-cols-1 md:grid-cols-2 lg:grid-cols-2 w-full  ml-7'>
                 <div className="space-y-2 my-8 mb-64 w-1/2">
                     
                     <div className=''>
                         <h2 className='text-2xl pb-3'>HTML</h2>
-                        <div className="w-5/6 bg-white h-7 mb-6 hidden md:block lg:block">
+                        <div className="w-11/12 bg-white h-7 mb-6 hidden md:block lg:block">
                             <div className="bg-theme h-7" style={{width: 52 + '%'}}></div>
                         </div>
                     </div>
 
                     <div className=''>
                         <h2 className='text-2xl pb-3'>CSS</h2>
-                        <div className="w-5/6 bg-white h-7 mb-6 hidden md:block lg:block">
+                        <div className="w-11/12 bg-white h-7 mb-6 hidden md:block lg:block">
                             <div className="bg-theme h-7" style={{width: 45 + '%'}}></div>
                         </div>
                     </div>
 
                     <div className=''>
                         <h2 className='text-2xl pb-3'>Javascript</h2>
-                        <div className="w-5/6 bg-white h-7 mb-6 hidden md:block lg:block">
+                        <div className="w-11/12 bg-white h-7 mb-6 hidden md:block lg:block">
                             <div className="bg-theme h-7" style={{width: 50 + '%'}}></div>
                         </div>
                     </div>
 
                     <div className=''>
                         <h2 className='text-2xl pb-3'>Python 3</h2>
-                        <div className="w-5/6 bg-white h-7 mb-6 hidden md:block lg:block">
+                        <div className="w-11/12 bg-white h-7 mb-6 hidden md:block lg:block">
                             <div className="bg-theme h-7" style={{width: 59 + '%'}}></div>
                         </div>
                     </div>
 
                     <div className=''>
                         <h2 className='text-2xl pb-3'>Django</h2>
-                        <div className="w-5/6 bg-white h-7 mb-6 hidden md:block lg:block">
+                        <div className="w-11/12 bg-white h-7 mb-6 hidden md:block lg:block">
                             <div className="bg-theme h-7" style={{width: 53 + '%'}}></div>
                         </div>
                     </div>
 
                     <div className=''>
                         <h2 className='text-2xl pb-3'>Java</h2>
-                        <div className="w-5/6 bg-white h-7 mb-6 hidden md:block lg:block">
+                        <div className="w-11/12 bg-white h-7 mb-6 hidden md:block lg:block">
                             <div className="bg-theme h-7" style={{width: 28 + '%'}}></div>
                         </div>
                     </div>
 
                     <div className=''>
                         <h2 className='text-2xl pb-3'>Selenium</h2>
-                        <div className="w-5/6 bg-white h-7 mb-6 hidden md:block lg:block">
+                        <div className="w-11/12 bg-white h-7 mb-6 hidden md:block lg:block">
                             <div className="bg-theme h-7" style={{width: 67 + '%'}}></div>
                         </div>
                     </div>
@@ -66,49 +66,49 @@ const Skills = () => {
                     
                     <div className=''>
                         <h2 className='text-2xl pb-3'>JIRA</h2>
-                        <div className="w-5/6 bg-white h-7 mb-6 hidden md:block lg:block">
+                        <div className="w-11/12 bg-white h-7 mb-6 hidden md:block lg:block">
                             <div className="bg-theme h-7" style={{width: 51 + '%'}}></div>
                         </div>
                     </div>
 
                     <div className=''>
                         <h2 className='text-2xl pb-3'>Git</h2>
-                        <div className="w-5/6 bg-white h-7 mb-6 hidden md:block lg:block">
+                        <div className="w-11/12 bg-white h-7 mb-6 hidden md:block lg:block">
                             <div className="bg-theme h-7" style={{width: 47 + '%'}}></div>
                         </div>
                     </div>
 
                     <div className=''>
                         <h2 className='text-2xl pb-3'>Bootstrap</h2>
-                        <div className="w-5/6 bg-white h-7 mb-6 hidden md:block lg:block">
+                        <div className="w-11/12 bg-white h-7 mb-6 hidden md:block lg:block">
                             <div className="bg-theme h-7" style={{width: 51 + '%'}}></div>
                         </div>
                     </div>
 
                     <div className=''>
                         <h2 className='text-2xl pb-3'>Next.js</h2>
-                        <div className="w-5/6 bg-white h-7 mb-6 hidden md:block lg:block">
+                        <div className="w-11/12 bg-white h-7 mb-6 hidden md:block lg:block">
                             <div className="bg-theme h-7" style={{width: 27 + '%'}}></div>
                         </div>
                     </div>
 
                     <div className=''>
                         <h2 className='text-2xl pb-3'>Tailwind CSS</h2>
-                        <div className="w-5/6 bg-white h-7 mb-6 hidden md:block lg:block">
+                        <div className="w-11/12 bg-white h-7 mb-6 hidden md:block lg:block">
                             <div className="bg-theme h-7" style={{width: 33 + '%'}}></div>
                         </div>
                     </div>
 
                     <div className=''>
                         <h2 className='text-2xl pb-3'>SQL</h2>
-                        <div className="w-5/6 bg-white h-7 mb-6 hidden md:block lg:block">
+                        <div className="w-11/12 bg-white h-7 mb-6 hidden md:block lg:block">
                             <div className="bg-theme h-7" style={{width: 30 + '%'}}></div>
                         </div>
                     </div>
 
                     <div className=''>
                         <h2 className='text-2xl pb-3'>Docker</h2>
-                        <div className="w-5/6 bg-white h-7 mb-6 hidden md:block lg:block">
+                        <div className="w-11/12 bg-white h-7 mb-6 hidden md:block lg:block">
                             <div className="bg-theme h-7" style={{width: 19 + '%'}}></div>
                         </div>
                     </div>
